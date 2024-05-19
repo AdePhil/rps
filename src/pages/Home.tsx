a0d054b8ddaebe06ebbc7e7012e004f9d07d05be
Ahmed Adebisi <adebisi.ahmed10@gmail.com>
2024-05-19 12:51:08 +0100

    style: add tailwind css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
-    <div>
-      <h1>Rock Paper scissors</h1>
+    <div className="bg-primary min-h-screen text-white py-10 px-3">
+      <h1 className="text-3xl text-center">Rock Paper scissors</h1>
     </div>
   );
 };
